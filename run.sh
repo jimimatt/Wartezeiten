7eb9ad082f0d0c36928c15114bc7944a738d8791
Matthias <matthias.teloeken@gmail.de>
2018-05-12 14:35:35 +0200

    run Skript überarbeitet

--- run.sh
+++ run.sh
@@ -1,14 +1,33 @@
-INPUT_FILE_ENDING="in"
-OUTPUT_FILE_ENDING="out"
-PATH_TO_DATA=""
-
-if [ $1 ]
+#!/bin/bash
+# check if jar file is present
+JAR=Wartezeiten.jar
+if [ ! -f $JAR ]
   then
-    PATH_TO_DATA=$1/
+    echo "$JAR not found!"
+    exit 1
 fi
-
-for f in $PATH_TO_DATA*.$INPUT_FILE_ENDING
+DIR="$1"
+ 
+# failsafe - fall back to current directory
+[ "$DIR" == "" ] && DIR="." || DIR="./$DIR"
+ 
+# save and change IFS
+OLDIFS=$IFS
+IFS=$'\n'
+ 
+# read all file name into an array
+fileArray=($(ls $DIR | grep -i ".in$"))
+ 
+# restore it
+IFS=$OLDIFS
+ 
+# get length of an array
+tLen=${#fileArray[@]}
+ 
+# use for loop read all filenames
+for (( i=0; i<${tLen}; i++ ));
 do
-  java -jar Wartezeiten.jar $f ${f%.*}.$OUTPUT_FILE_ENDING
+#  echo "${fileArray[$i]}"
+#  echo "${fileArray[$i]%.*}.out"
+  java -jar $JAR "$DIR/${fileArray[$i]}" "$DIR/${fileArray[$i]%.*}.out"
 done
-
